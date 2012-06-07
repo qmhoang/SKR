9516542fb5e8c0a9bf877e75398a48f306c20744
Quang-Minh Hoang <Quang-Minh Hoang@localhost>
2012-06-07 00:44:24 -0500

    minor test added to see if world's utility functions are working

---
+++ SKRTests/WorldTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SKR.Universe;
+
+namespace SKRTests {
+    [TestFixture]
+    class WorldTest {
+        [TestCase(100)]
+        [TestCase(50)]
+        [TestCase(200)]
+        [TestCase(150)]
+        public void TestStaticConversion(int value) {
+            Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(value)), value);
+        }
+    }
+}
