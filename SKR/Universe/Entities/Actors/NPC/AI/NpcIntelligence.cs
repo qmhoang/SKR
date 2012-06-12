7997f775a6eb523341846383a4e2c197e4aab8b8
Quang-Minh Hoang <Quang-Minh Hoang@localhost>
2012-06-12 04:11:29 -0500

    fixed simple npc ai

--- SKR/Universe/Entities/Actors/NPC/AI/NpcIntelligence.cs
+++ SKR/Universe/Entities/Actors/NPC/AI/NpcIntelligence.cs
@@ -11,7 +11,7 @@ namespace SKR.Universe.Entities.Actors.NPC.AI {
         public virtual void Update() { }
 
         public override float getWalkCost(int xFrom, int yFrom, int xTo, int yTo) {
-            if (!Actor.Level.IsWalkable(xTo, yTo) || Actor.Level.DoesActorExistAtLocation(xTo, yTo)) {
+            if (!Actor.Level.IsWalkable(xTo, yTo)) {
                 return 0f;
             }
             return 1f;
