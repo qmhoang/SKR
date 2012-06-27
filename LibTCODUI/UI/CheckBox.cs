9310107fffac865d8e78f9c82ffb630ad3b49835
Quang-Minh Hoang <Quang-Minh Hoang@localhost>
2012-06-27 02:25:20 -0500

    UI is its own project

---
+++ LibTCODUI/UI/CheckBox.cs
@@ -0,0 +1,324 @@
+﻿//Copyright (c) 2010 Shane Baker
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+//
+
+using System;
+using DEngine.Core;
+using libtcod;
+
+namespace DEngine.UI
+{
+    /// <summary>
+    /// Contains the data needed to construct a CheckBox object.  A CheckBox will, by default, automatically
+    /// generate its width based on the Label and MinimumWidth properties of the template, leaving space for
+    /// the check element, and will always
+    /// have a height of 3 (1 space for the label and 2 spaces for the borders).
+    /// </summary>
+    public class CheckBoxTemplate : ControlTemplate
+    {
+
+        /// <summary>
+        /// Default constructor, sets all properties to the default.
+        /// </summary>
+        public CheckBoxTemplate()
+        {
+            this.Label = "";
+            this.MinimumWidth = 0;
+            LabelAlignment = HorizontalAlignment.Left;
+            CheckOnLeft = true;
+            HilightWhenMouseOver = false;
+            CanHaveKeyboardFocus = false;
+            HasFrameBorder = true;
+            VerticalAlign = VerticalAlignment.Center;
+        }
+
+
+
+        /// <summary>
+        /// Set this to manually size the check box.  If empty (the default), then the 
+        /// check box will be autosized.
+        /// </summary>
+        public Size AutoSizeOverride { get; set; }
+
+        /// <summary>
+        /// The text displayed by the checkbox.  Defaults to empty string ""
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// The minimum width of the button when autosizing.  This property is ignored if MinimumWidth is less 
+        /// than the automatically calculated size.  Defaults to 0.
+        /// </summary>
+        public int MinimumWidth { get; set; }
+
+        /// <summary>
+        /// If there is extra spacing in the label area, the label gets aligned
+        /// according to this.  Defautls to HorizontalAlignment.Left
+        /// </summary>
+        public HorizontalAlignment LabelAlignment { get; set; }
+
+        /// <summary>
+        /// The vertical alignment of the label and check element.  Defaults to
+        /// VerticalAlignment.Center
+        /// </summary>
+        public VerticalAlignment VerticalAlign { get; set; }
+
+        /// <summary>
+        /// If true, the check field is placed left of the label, otherwise on the 
+        /// right.  Defaults to true.
+        /// </summary>
+        public bool CheckOnLeft { get; set; }
+
+        /// <summary>
+        /// True if the checkbox will draw itself with hilight colors when under the mouse
+        /// pointer.  Defaults to false.
+        /// </summary>
+        public bool HilightWhenMouseOver { get; set; }
+
+        /// <summary>
+        /// Set to true if this control can take the keyboard focus.
+        /// Defaults to false.
+        /// </summary>
+        public bool CanHaveKeyboardFocus { get; set; }
+
+        /// <summary>
+        /// If true, a border will be drawn around the check box.  If autosizing, space for the
+        /// frame will be added.  Defaults to true.
+        /// </summary>
+        public bool HasFrameBorder { get; set; }
+
+        /// <summary>
+        /// Auto generates the size of the control based on the other options.
+        /// </summary>
+        /// <returns></returns>
+        public override Size CalculateSize()
+        {
+            if (!AutoSizeOverride.IsEmpty)
+            {
+                return AutoSizeOverride;
+            }
+
+            int width = Canvas.TextLength(Label) + 1;
+            int height = 1;
+
+            if (HasFrameBorder)
+            {
+                width += 2;
+                height += 2;
+            }
+            width = Math.Max(width, MinimumWidth);
+
+            return new Size(width, height);
+        }
+
+    }
+
+    /// <summary>
+    /// Represents a check box control.  A CheckBox has a label and a checkable element that displays the 
+    /// current state of the IsChecked property.  This state
+    /// is toggled by left mouse button clicks, or by setting the IsChecked property manually.
+    /// </summary>
+    public class CheckBox : Control
+    {
+        #region Events
+        /// <summary>
+        /// Raised when the state of a checkbox has been toggled by user input.  Get IsChecked to get
+        /// current state.  Manually setting IsChecked property will not cause this event to be raised.
+        /// </summary>
+        public event EventHandler CheckBoxToggled;
+        #endregion
+        #region Constructors
+
+        /// <summary>
+        /// Construct a CheckBox instance from the given template.
+        /// </summary>
+        /// <param name="template"></param>
+        public CheckBox(CheckBoxTemplate template)
+            : base(template)
+        {
+            HasFrame = template.HasFrameBorder;
+
+            if (Size.Height < 3 || Size.Width < 3)
+            {
+                HasFrame = false;
+            }
+
+            HilightWhenMouseOver = template.HilightWhenMouseOver;
+            CanHaveKeyboardFocus = template.CanHaveKeyboardFocus;
+
+            this.Label = template.Label;
+            if (Label == null)
+                Label = "";
+
+            this.CheckOnLeft = template.CheckOnLeft;
+            this.LabelAlignment = template.LabelAlignment;
+            this.VerticalAlign = template.VerticalAlign;
+
+            CalcMetrics(template);
+
+        }
+
+        #endregion
+        #region Public Properties
+
+        /// <summary>
+        /// Get or set the current checked state of the checkbox.  Setting this property will
+        /// not raise a CheckBoxToggled event.
+        /// </summary>
+        public bool IsChecked { get; set; }
+
+        /// <summary>
+        /// Get the label string
+        /// </summary>
+        public string Label { get; private set; }
+
+        /// <summary>
+        /// True if the check element appears left of the label, otherwise on right side
+        /// </summary>
+        public bool CheckOnLeft { get; private set; }
+
+        /// <summary>
+        /// Text alignment of the label
+        /// </summary>
+        public HorizontalAlignment LabelAlignment { get; private set; }
+
+        /// <summary>
+        /// The vertical alignment of the label and check element.
+        /// </summary>
+        public VerticalAlignment VerticalAlign { get; private set; }
+
+        #endregion
+        #region Message Handlers
+
+        /// <summary>
+        /// Triggers appropriate events based on a mouse button down action.  Override to
+        /// add custom mouse button handling code.
+        /// </summary>
+        /// <param name="mouseData"></param>
+        protected internal override void OnMouseButtonDown(MouseData mouseData)
+        {
+            base.OnMouseButtonDown(mouseData);
+
+            if (mouseData.MouseButton == MouseButton.LeftButton)
+            {
+                if (IsChecked)
+                {
+                    IsChecked = false;
+                }
+                else
+                {
+                    IsChecked = true;
+                }
+
+                if (CheckBoxToggled != null)
+                {
+                    CheckBoxToggled(this, EventArgs.Empty);
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Draws the label and chec element based on current state.  Override to add custom
+        /// drawing code.
+        /// </summary>
+        protected override void Redraw()
+        {
+            base.Redraw();
+
+            if (!string.IsNullOrEmpty(Label))
+            {
+                Canvas.PrintStringAligned(labelRect,
+                    Label,
+                    LabelAlignment,
+                    VerticalAlign);
+            }
+
+            if (IsActive)
+            {
+                if (IsChecked)
+                {
+                    Canvas.PrintChar(checkPos, 225, Pigments[PigmentType.ViewNormal]);
+                }
+                else
+                {
+                    Canvas.PrintChar(checkPos, 224, Pigments[PigmentType.ViewNormal]);
+                }
+            }
+        }
+
+        #endregion
+        #region Private 
+
+        //private int labelPosX;
+        //private int labelFieldLength;
+        //private int labelPosY;
+        private Rect labelRect;
+        private Point checkPos;
+
+        private void CalcMetrics(CheckBoxTemplate template)
+        {
+            Rect inner = this.LocalRect;
+
+            if (template.HasFrameBorder && template.CalculateSize().Height >= 3)
+            {
+                inner = Rect.Inflate(inner, -1, -1);
+            }
+
+            int checkX;
+
+            if (CheckOnLeft)
+            {
+                checkX = inner.Left;
+                labelRect = new Rect(inner.TopLeft.Shift(1, 0),
+                    inner.BottomRight);
+            }
+            else
+            {
+                checkX = inner.Right;
+                labelRect = new Rect(inner.TopLeft,
+                    inner.BottomRight.Shift(-1, 0));
+            }
+
+            if (labelRect.Size.Width < 1)
+            {
+                Label = "";
+            }
+
+            switch (VerticalAlign)
+            {
+                case VerticalAlignment.Bottom:
+                    checkPos = new Point(checkX, labelRect.Bottom);
+                    break;
+
+                case VerticalAlignment.Center:
+                    checkPos = new Point(checkX, labelRect.Center.Y);
+                    break;
+
+                case VerticalAlignment.Top:
+                    checkPos = new Point(checkX, labelRect.Top);
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ LibTCODUI/UI/SpinBox.cs
@@ -0,0 +1,333 @@
+﻿//Copyright (c) 2010 Shane Baker
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+//
+
+using System;
+using DEngine.Core;
+
+namespace DEngine.UI
+{
+    public class SpinBoxTemplate : ControlTemplate
+    {
+        public SpinBoxTemplate()
+        {
+            MaximumValue = 1;
+            MaximumValue = 0;
+            Label = "";
+        }
+
+        /// <summary>
+        /// The minimum value that this spin control can have.  Defaults to 0.
+        /// </summary>
+        public int MinimumValue { get; set; }
+
+        /// <summary>
+        /// The maximum value that this spin control can have.  Defaults to 1.
+        /// </summary>
+        public int MaximumValue { get; set; }
+
+        /// <summary>
+        /// The delay in milliseconds after first clicking on a spin button before
+        /// the spin cycle starts.  Defaults to 0.
+        /// </summary>
+        public uint SpinDelay { get; set; }
+
+        /// <summary>
+        /// The speed of the spin cycle.  This is measure in the millisecond delay before
+        /// each step; thus, a smaller number here means a faster spin.  Defaults to 0.
+        /// </summary>
+        public uint SpinSpeed { get; set; }
+
+        /// <summary>
+        /// And optional label to display to the left of the numerical entry and spin buttons.  Defaults
+        /// to empty string (no label).
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// The value that the spin box will initially have.  Defaults to 0.
+        /// </summary>
+        public int StartingValue { get; set; }
+
+        public override Size CalculateSize()
+        {
+            int width = 2; // for frame
+
+            if (!string.IsNullOrEmpty(Label))
+            {
+                width += Canvas.TextLength(Label) + 1;
+            }
+
+            width += NumberEntryTemplate.CalculateFieldWidth(MaximumValue, MinimumValue);
+            width += 3; // for buttons
+
+            return new Size(width, 3);
+        }
+    }
+
+    public class SpinBox : Control
+    {
+        #region Events
+
+        public event EventHandler ValueChanged;
+
+        #endregion
+        #region Constructors
+
+        public SpinBox(SpinBoxTemplate template)
+            : base(template)
+        {
+            MinimumValue = template.MinimumValue;
+            MaximumValue = template.MaximumValue;
+            SpinDelay = template.SpinDelay;
+            SpinSpeed = template.SpinSpeed;
+            Label = template.Label;
+
+            if (Label == null)
+            {
+                Label = "";
+            }
+
+            CurrentValue = template.StartingValue;
+            if (CurrentValue < MinimumValue || CurrentValue > MaximumValue)
+            {
+                CurrentValue = MinimumValue;
+            }
+
+
+
+            HasFrame = true;
+            CanHaveKeyboardFocus = false;
+            HilightWhenMouseOver = false;
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The current value of the spin box.
+        /// </summary>
+        public int CurrentValue
+        {
+            get { return _currentValue; }
+            set
+            {
+                int newVal = value;
+
+                if (newVal < MinimumValue)
+                    newVal = MinimumValue;
+
+                if (newVal > MaximumValue)
+                    newVal = MaximumValue;
+
+                if (newVal != _currentValue)
+                {
+                    _currentValue = value;
+                }
+            }
+        }
+        private int _currentValue;
+        #endregion
+
+        #region Protected Properties
+
+        /// <summary>
+        /// The minimum value that this spin control can have.  Defaults to 0.
+        /// </summary>
+        protected int MinimumValue { get; private set; }
+
+        /// <summary>
+        /// The delay in milliseconds after first clicking on a spin button before
+        /// the spin cycle starts.  Defaults to 0.
+        /// </summary>
+        protected int MaximumValue { get; private set; }
+
+        /// <summary>
+        /// The delay in milliseconds after first clicking on a spin button before
+        /// the spin cycle starts.  Defaults to 0.
+        /// </summary>
+        protected uint SpinDelay { get; set; }
+
+        /// <summary>
+        /// And optional label to display to the left of the numerical entry and spin buttons.  Defaults
+        /// to empty string (no label).
+        /// </summary>
+        protected uint SpinSpeed { get; set; }
+
+        /// <summary>
+        /// And optional label to display to the left of the numerical entry and spin buttons.  Defaults
+        /// to empty string (no label).
+        /// </summary>
+        protected string Label { get; private set; }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected virtual void OnValueChanged()
+        {
+            if (ValueChanged != null)
+            {
+                ValueChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected internal override void OnSettingUp()
+        {
+            base.OnSettingUp();
+
+            if (!string.IsNullOrEmpty(Label))
+            {
+                labelRect = new Rect(1, 1, Label.Length + 1, 1);
+                upButtonPos = new Point(Label.Length + 2, 1) + ActualScreenPosition;
+            }
+            else
+            {
+                upButtonPos = Point.One + ActualScreenPosition;
+            }
+
+            int fieldWidth = NumberEntryTemplate.CalculateFieldWidth(MaximumValue, MinimumValue);
+            Size fieldSize = new Size(fieldWidth, 1);
+            fieldRect = new Rect(upButtonPos.Shift(2, 0), fieldSize);
+
+            downButtonPos = fieldRect.TopRight.Shift(1, 0);
+            
+            numEntry = new NumberEntry(new NumberEntryTemplate()
+            {
+                HasFrameBorder = false,
+                MinimumValue = this.MinimumValue,
+                MaximumValue = this.MaximumValue,
+                StartingValue = CurrentValue,
+                CommitOnLostFocus = true,
+                ReplaceOnFirstKey = true,
+                TopLeftPos = fieldRect.TopLeft
+            });
+
+            upButton = new EmitterButton(new EmitterButtonTemplate()
+            {
+                HasFrameBorder = false,
+                Label = ((char)libtcod.TCODSpecialCharacter.ArrowNorthNoTail).ToString(),
+                TopLeftPos = upButtonPos,
+                StartEmittingDelay = SpinDelay,
+                Speed = SpinSpeed
+            });
+
+            downButton = new EmitterButton(new EmitterButtonTemplate()
+            {
+                HasFrameBorder = false,
+                Label = ((char)libtcod.TCODSpecialCharacter.ArrowSouthNoTail).ToString(),
+                TopLeftPos = downButtonPos,
+                StartEmittingDelay = SpinDelay,
+                Speed = SpinSpeed
+            });
+
+            ParentWindow.AddControls(downButton, upButton, numEntry);
+
+            upButton.Emit += new EventHandler(upButton_Emit);
+            downButton.Emit += new EventHandler(downButton_Emit);
+            numEntry.EntryChanged += new EventHandler(numEntry_EntryChanged);
+        }
+
+        protected override void Redraw()
+        {
+            base.Redraw();
+            if (!string.IsNullOrEmpty(Label))
+            {
+                Canvas.PrintString(labelRect.TopLeft, Label);
+
+                Canvas.PrintChar(labelRect.TopRight.Shift(0, -1),
+                    (int)libtcod.TCODSpecialCharacter.TeeSouth,
+                    DetermineFramePigment());
+
+                Canvas.PrintChar(labelRect.TopRight.Shift(0, 0),
+                    (int)libtcod.TCODSpecialCharacter.VertLine,
+                    DetermineFramePigment());
+
+                Canvas.PrintChar(labelRect.TopRight.Shift(0, 1),
+                    (int)libtcod.TCODSpecialCharacter.TeeNorth,
+                    DetermineFramePigment());
+
+            }
+        }
+        #endregion
+        #region Private
+
+        void numEntry_EntryChanged(object sender, EventArgs e)
+        {
+            NumberEntry entry = sender as NumberEntry;
+
+            if(this.CurrentValue != entry.CurrentValue)
+            {
+                this.CurrentValue = entry.CurrentValue;
+                OnValueChanged();
+            }
+        }
+
+        void downButton_Emit(object sender, EventArgs e)
+        {
+            numEntry.TryCommit();
+            if (CurrentValue > MinimumValue)
+            {
+                numEntry.TrySetValue(CurrentValue - 1);
+            }
+        }
+
+        void upButton_Emit(object sender, EventArgs e)
+        {
+            numEntry.TryCommit();
+            if (CurrentValue < MaximumValue)
+            {
+                numEntry.TrySetValue(CurrentValue + 1);
+            }
+        }
+
+        NumberEntry numEntry;
+        EmitterButton downButton;
+        EmitterButton upButton;
+        Rect fieldRect;
+        Rect labelRect;
+        Point upButtonPos;
+        Point downButtonPos;
+
+        #endregion
+        #region Dispose
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            if (isDisposing)
+            {
+                if (numEntry != null)
+                    numEntry.Dispose();
+
+                if (upButton != null)
+                    upButton.Dispose();
+
+                if (downButton != null)
+                    downButton.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
