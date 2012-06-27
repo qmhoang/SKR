6aab16d7ceb21b5f680b3e678875260a9270d141
Quang-Minh Hoang <Quang-Minh Hoang@localhost>
2012-06-27 04:36:10 -0500

    clean up

---
+++ SKR/Universe/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DEngine.Actor;
+using DEngine.Core;
+using DEngine.Entities;
+
+namespace SKR.Universe.Entities {
+    public class GameEntity : EngineEntity {
+        public GameEntity(RefId refId) : base(refId) {}
+        public GameEntity(RefId refId, Point position, Dictionary<ComponentType, EntityComponent> components) : base(refId, position, components) { }
+    }
+}
