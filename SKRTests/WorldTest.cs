0ceb18894eece887443c14b29ab598a66326040e
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2012-07-07 23:09:23 -0500

    renamed skrtest to skrgametest, change dependencies

--- SKRTests/WorldTest.cs
+++ SKRTests/WorldTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using SKR.Universe;
+using SkrGame.Universe;
 
 namespace SKRTests {
     [TestFixture]
