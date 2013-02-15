3552ba23d063b67a4de53df93cb8ae2c23f29526
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2013-02-15 14:52:54 -0600

    * removed npc/ai stuff, now uses npc actor * added leveltests * walkactiontests * worldtests (from abstractworld tests) * fixed blockers constructor * fixed location's positionchangedevent * simplified world's constructor and moved into skrapp (for now) * removed longaction (no longer necessary)

---
+++ SKRTests/WorldTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SkrGame.Universe;
+
+namespace SKRTests {
+	[TestFixture]
+	class WorldTest {
+		
+		[Test]
+		public static void APFunctions() {
+			// an action that takes 2 second should equal 200  AP
+			Assert.AreEqual(World.SecondsToActionPoints(2), 200);
+
+			Assert.AreEqual(World.SecondsToSpeed(10), 10);
+
+			Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(5)), 5);
+			Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(9)), 9);
+			Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(50)), 50);
+			Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(14)), 14);
+		}
+
+		[TestCase(100)]
+		[TestCase(50)]
+		[TestCase(200)]
+		[TestCase(150)]
+		public void StaticConversion(int value) {
+			Assert.AreEqual(World.SecondsToSpeed(World.SpeedToSeconds(value)), value);
+			Assert.AreEqual(World.SecondsToActionPoints(World.SpeedToSeconds(World.ActionPointsToSpeed(value))), value);
+		}
+	}
+}
